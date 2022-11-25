a8f88ae9009da1f19461b4900013ae6363ab48ca
Giuseppe Sgroi <giuseppe.sgroi@ericsson.com>
2022-11-25 11:30:46 +0100

    the first commit

---
+++ README.md
@@ -0,0 +1,118 @@
+# Seplos MQTT
+Read data From Seplos BMS and send them to the Home Assistant
+
+This is a bash script that read data from Seplos BMS via RS485 port and send the data to the Home Assistan via MQTT.
+
+## Hardware requirements:
+1. Raspberry (i use an RPI4)
+2. USB to RS485 adapter
+3. Home assitant with MQTT broker
+
+## Installation and configuration
+
+Prepare Raspberry with Raspberry PI OS
+perform the apt-get update and the apt-get upgrade
+
+move to the your user home and use git clone to download this script
+
+```
+git clone https://github.com/byte4geek/SEPLOS_MQTT.git
+
+chmod 700 ~/SEPLOS_MQTT/query_seplos_ha.sh ~/SEPLOS_MQTT/run_bms_query.sh
+```
+
+edit the script ```~/SEPLOS_MQTT/query_seplos_ha.sh``` and set the COM port that you use (Ex. DEV=/dev/ttyUSB0)
+
+edit the script ```~/SEPLOS_MQTT/run_bms_query.sh``` and set the below parameters with your MQTT server information:
+
+```
+# insert the mqtt info below
+# mqtt host name
+MQTTHOST=192.168.1.2
+# name inserted into topic
+TOPIC=seplos
+# mqtt user name
+MQTTUSER=mqttuser
+# mqtt password
+MQTTPASWD=mqttpassword
+# time to read and update datas vs mqtt server and Home Assistant
+TELEPERIOD=10
+# is a prefix inserted into topic, chage it if you need
+id_prefix=364715398511
+# Max size of the BMS_error.log and nohup.out
+MAXSIZE=2000000
+```
+
+then install the following pkg:
+
+```
+sudo apt-get install jq bc mosquitto-clients
+```
+
+edit the crontab to run the script at the boot
+
+```crontab -e``` and add the line below:
+```
+@reboot nohup ~/SEPLOS_MQTT/run_bms_query.sh &
+```
+## Manual execution
+simply run 
+```~/SEPLOS_MQTT/run_bms_query.sh```
+or
+```nohup ~/SEPLOS_MQTT/run_bms_query.sh &```
+
+To test if the communication is working try to run
+```~/SEPLOS_MQTT/query_seplos_ha.sh 4201```
+
+you can see the output like this:
+```
+3334
+3334
+3334
+3335
+3334
+3335
+3334
+3335
+3335
+3336
+3335
+3335
+3335
+3335
+3335
+3334
+31.7
+32.2
+32.0
+31.8
+36.5
+33.7
+0
+53.35
+273.97
+280.00
+97.8
+280.00
+12
+100.0
+54.45
+```
+
+When the script run, it sends an MQTT message like this:
+
+```
+homeassistant/sensor/seplos_364715398511 {"lowest_cell":"Cell 8 - 3427 mV","highest_cell":"Cell 7 - 3435 mV","difference":"8","cell01":"3431","cell02":"3431","cell03":"3434","cell04":"3430","cell05":"3433","cell06":"3432","cell07":"3435","cell08":"3427","cell09":"3431","cell10":"3428","cell11":"3433","cell12":"3433","cell13":"3435","cell14":"3431","cell15":"3435","cell16":"3428","cell_temp1":"31.7","cell_temp2":"32.2","cell_temp3":"32.0","cell_temp4":"31.9","env_temp":"37.2","power_temp":"34.9","charge_discharge":"26.01","total_voltage":"54.90","residual_capacity":"271.24","soc":"96.8","cycles":"12","soh":"100.0","port_voltage":"54.93"}
+```
+
+## Configuring Home Assistant
+
+Based on the MQTT message then create all MQTT sensors and the template sensors using the configuration.yaml file and add all sensors to the Home Assistant dashboard using the lovelace.yaml file
+
+example:
+![BMS dashboard](https://github.com/byte4geek/Seplos-BMS-vs-Home-Assistant/raw/main/bms_ha_panel.JPG)
+
+# Donation
+Buy me a coffee
+
+[![Donate](https://img.shields.io/badge/Donate-PayPal-green.svg)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=VK4CSX9NVQAZU)
---
+++ run_bms_query.sh
@@ -0,0 +1,137 @@
+#!/bin/bash
+# insert the mqtt info below
+# mqtt host name
+MQTTHOST=192.168.1.2
+# name inserted into topic
+TOPIC=seplos
+# mqtt user name
+MQTTUSER=your_mqtt_user
+# mqtt password
+MQTTPASWD=your_mqtt_password
+# time to read and update datas vs mqtt server and Home Assistant
+TELEPERIOD=10
+# is a prefix inserted into topic, chage it if you need
+id_prefix=364715398511
+# Max size of the BMS_error.log and nohup.out
+MAXSIZE=2000000
+# Log file name
+LOGNAME=~/SEPLOS_MQTT/BMS_error.log
+# nohup file name for standard and error output
+NOUPFILE=~/SEPLOS_MQTT/nohup.out
+
+# The script....
+
+for (( ; ; ))
+do
+  LOGNAME_SIZE=$(ls -l $LOGNAME | awk '{print $5}')
+  if [ $LOGNAME_SIZE -ge $MAXSIZE ]; then
+    mv $LOGNAME $LOGNAME.old
+  fi
+  
+  NOUPFILE_SIZE=$(ls -l $NOUPFILE | awk '{print $5}')
+  if [ $NOUPFILE_SIZE -ge $MAXSIZE ]; then
+    cp $NOUPFILE $NOUPFILE.old
+    cat /dev/null > $NOUPFILE
+  fi
+
+   QUERY=$(~/SEPLOS_MQTT/query_seplos_ha.sh 4201)
+   
+# Find lowest and high value
+               onlycells=$(echo $QUERY|awk '{print $1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16}')
+
+#echo ${onlycells[@]}
+               lowcell=$(echo ${onlycells[@]} | awk 'BEGIN{RS=" ";} {print $1}' | sort| sed -n 1p)
+               highcell=$(echo ${onlycells[@]} | awk 'BEGIN{RS=" ";} {print $1}' | sort| sed -n 16p)
+               
+# calulate difference
+               DIFF=`bc -l <<< $highcell-$lowcell`
+
+# find the number of the lowest and highest cell
+lowcellnumb=$(echo ${onlycells[@]} | awk 'BEGIN{RS=" ";} {print $1}' |nawk '{print $0, FNR}'| sort|sed -n 1p|awk '{print $2}')
+highcellnumb=$(echo ${onlycells[@]} | awk 'BEGIN{RS=" ";} {print $1}' |nawk '{print $0, FNR}'| sort|sed -n 16p|awk '{print $2}')
+
+# Working on the bad data from the query script and skip send it to Mqtt server
+VAR="$(echo $QUERY|awk '{print $27}')"
+#VAR=${VAR/./,}
+#echo "$(date) - $VAR - $onlycells" >> $LOGNAME
+#    if [[ "$VAR" -gt 101 && "$VAR" -lt 0 ]]; then
+    if [[ "$onlycells" =~ "rror" ]]; then
+      echo "$(date) - Possible bad read data from BMS - Error 1" >> $LOGNAME
+#      echo "$(date) - Possible bad read data from BMS - Error 1"
+    else
+    
+      if [[ "$onlycells" =~ "Failed" ]]; then
+        echo "$(date) - Possible bad read data from BMS - Error 2" >> $LOGNAME
+#        echo "$(date) - Possible bad read data from BMS - Error 2"
+      else
+      
+        if (( $(echo "$VAR"'>'100 |bc -l) )); then
+          echo "$(date) - Possible bad read data from BMS - Error 3" >> $LOGNAME
+#          echo "$(date) - Possible bad read data from BMS - Error 3"
+        else
+        
+          if (( $(echo "$VAR"'<'1 |bc -l) )); then
+            echo "$(date) - Possible bad read data from BMS - Error 4" >> $LOGNAME
+#            echo "$(date) - Possible bad read data from BMS - Error 4"
+          else
+          
+            if [[ "$onlycells" =~ "~" ]]; then
+              echo "$(date) - Possible bad read data from BMS - Error 5" >> $LOGNAME
+#              echo "$(date) - Possible bad read data from BMS - Error 5"
+            else
+            
+              if [ "${VAR+x}" = x ] && [ -z "$VAR" ]; then
+                echo "$(date) - Possible bad read data from BMS - Error 6" >> $LOGNAME
+#                echo "$(date) - Possible bad read data from BMS - Error 6"
+              else
+
+# prepare MQTT argument
+mqtt_argument=$(printf "{\
+\"lowest_cell\":\"Cell $lowcellnumb - $lowcell mV\",\
+\"lowest_cell_v\":\"$lowcell\",\
+\"lowest_cell_n\":\"$lowcellnumb\",\
+\"highest_cell\":\"Cell $highcellnumb - $highcell mV\",\
+\"highest_cell_v\":\"$highcell\",\
+\"highest_cell_n\":\"$highcellnumb\",\
+\"difference\":\"$DIFF\",\
+\"cell01\":\"$(echo $QUERY|awk '{print $1}')\",\
+\"cell02\":\"$(echo $QUERY|awk '{print $2}')\",\
+\"cell03\":\"$(echo $QUERY|awk '{print $3}')\",\
+\"cell04\":\"$(echo $QUERY|awk '{print $4}')\",\
+\"cell05\":\"$(echo $QUERY|awk '{print $5}')\",\
+\"cell06\":\"$(echo $QUERY|awk '{print $6}')\",\
+\"cell07\":\"$(echo $QUERY|awk '{print $7}')\",\
+\"cell08\":\"$(echo $QUERY|awk '{print $8}')\",\
+\"cell09\":\"$(echo $QUERY|awk '{print $9}')\",\
+\"cell10\":\"$(echo $QUERY|awk '{print $10}')\",\
+\"cell11\":\"$(echo $QUERY|awk '{print $11}')\",\
+\"cell12\":\"$(echo $QUERY|awk '{print $12}')\",\
+\"cell13\":\"$(echo $QUERY|awk '{print $13}')\",\
+\"cell14\":\"$(echo $QUERY|awk '{print $14}')\",\
+\"cell15\":\"$(echo $QUERY|awk '{print $15}')\",\
+\"cell16\":\"$(echo $QUERY|awk '{print $16}')\",\
+\"cell_temp1\":\"$(echo $QUERY|awk '{print $17}')\",\
+\"cell_temp2\":\"$(echo $QUERY|awk '{print $18}')\",\
+\"cell_temp3\":\"$(echo $QUERY|awk '{print $19}')\",\
+\"cell_temp4\":\"$(echo $QUERY|awk '{print $20}')\",\
+\"env_temp\":\"$(echo $QUERY|awk '{print $21}')\",\
+\"power_temp\":\"$(echo $QUERY|awk '{print $22}')\",\
+\"charge_discharge\":\"$(echo $QUERY|awk '{print $23}')\",\
+\"total_voltage\":\"$(echo $QUERY|awk '{print $24}')\",\
+\"residual_capacity\":\"$(echo $QUERY|awk '{print $25}')\",\
+\"soc\":\"$(echo $QUERY|awk '{print $27}')\",\
+\"cycles\":\"$(echo $QUERY|awk '{print $29}')\",\
+\"soh\":\"$(echo $QUERY|awk '{print $30}')\",\
+\"port_voltage\":\"$(echo $QUERY|awk '{print $31}')\"\
+}")
+# send MQTT message with all parameters
+        mosquitto_pub -h $MQTTHOST -u $MQTTUSER -P $MQTTPASWD -t "homeassistant/sensor/"$TOPIC"_"$id_prefix"" -m "$mqtt_argument"
+#        echo "mqtt sent"
+            fi
+          fi
+        fi
+      fi
+    fi
+  fi
+  sleep $TELEPERIOD
+done
