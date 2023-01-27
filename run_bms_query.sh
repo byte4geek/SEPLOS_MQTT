2fcda3356789de8ab43e7746f568179d8f9570d8
byte4geek <admin@byte4geek.com>
2023-01-27 15:58:34 +0100

    Update run_bms_query.sh
    added if the cells voltage is between min e max value specified in the config file to avoid the scrit send incorrect value to Mqtt server like 0 voltage for example

--- run_bms_query.sh
+++ run_bms_query.sh
@@ -1,41 +1,61 @@
 #!/bin/bash
-# insert the mqtt infos below
-# mqtt host name
-MQTTHOST=192.168.1.2
-# name inserted into topic
-TOPIC=seplos
-# mqtt user name
-MQTTUSER=your_mqtt_user
-# mqtt password
-MQTTPASWD=your_mqtt_password
-# time to read and update datas vs mqtt server and Home Assistant
-TELEPERIOD=10
-# is a prefix inserted into topic, chage it if you need
-id_prefix=364715398511
-# Max size of the BMS_error.log and nohup.out
-MAXSIZE=2000000
-# Log file name
-LOGNAME=~/SEPLOS_MQTT/BMS_error.log
-# nohup file name for standard and error output
-NOUPFILE=~/SEPLOS_MQTT/nohup.out
 
-# The script....
+# Config parameter Load
+MQTTHOST=$(grep "MQTTHOST" config.ini | awk -F "=" '{print $2}')
+TOPIC=$(grep "TOPIC" config.ini | awk -F "=" '{print $2}')
+MQTTUSER=$(grep "MQTTUSER" config.ini | awk -F "=" '{print $2}')
+MQTTPASWD=$(grep "MQTTPASWD" config.ini | awk -F "=" '{print $2}')
+TELEPERIOD=$(grep "TELEPERIOD" config.ini | awk -F "=" '{print $2}')
+id_prefix=$(grep "id_prefix" config.ini | awk -F "=" '{print $2}')
+MAXSIZE=$(grep "MAXSIZE" config.ini | awk -F "=" '{print $2}')
+CELL_MIN_VOLT=$(grep "CELL_MIN_VOLT" config.ini | awk -F "=" '{print $2}')
+CELL_MAX_VOLT=$(grep "CELL_MAX_VOLT" config.ini | awk -F "=" '{print $2}')
+
+# The function....
+
+checkcellsvoltage()
+{
+	 STATUS=0
+	 counter=1
+	 for CELLVOLTAGE in "$CELL1" "$CELL2" "$CELL3" "$CELL4" "$CELL5" "$CELL6" "$CELL7" "$CELL8" "$CELL9" "$CELL10" "$CELL11" "$CELL12" "$CELL13" "$CELL14" "$CELL15" "$CELL16"; do
+		if [ "$CELLVOLTAGE" -lt $CELL_MIN_VOLT ] || [ "$CELLVOLTAGE" -gt $CELL_MAX_VOLT ]; then
+			echo "$(date) - Error: The value $CELLVOLTAGE for cell "$counter" is not between "$CELL_MIN_VOLT" and "$CELL_MAX_VOLT"" >> $LOGNAME
+			STATUS=1
+			break
+		fi
+		((counter++))
+	done
+	return $STATUS
+}
+
+# The main script....
+
+LOGNAME=~/test/BMS_error.log
+NOUPFILE=~/test/nohup.out
+
+if [ ! -f "$LOGNAME" ]; then
+touch "$LOGNAME"
+fi
+
+if [ ! -f "$NOUPFILE" ]; then
+touch "$NOUPFILE"
+fi
 
 for (( ; ; ))
 do
-  LOGNAME_SIZE=$(ls -l $LOGNAME | awk '{print $5}')
+  LOGNAME_SIZE=$(ls -l "$LOGNAME" | awk '{print $5}')
   if [ $LOGNAME_SIZE -ge $MAXSIZE ]; then
-    mv $LOGNAME $LOGNAME.old
+    mv "$LOGNAME" "$LOGNAME".old
   fi
   
-  NOUPFILE_SIZE=$(ls -l $NOUPFILE | awk '{print $5}')
+  NOUPFILE_SIZE=$(ls -l "$NOUPFILE" | awk '{print $5}')
   if [ $NOUPFILE_SIZE -ge $MAXSIZE ]; then
-    cp $NOUPFILE $NOUPFILE.old
-    cat /dev/null > $NOUPFILE
+    cp "$NOUPFILE" "$NOUPFILE".old
+    cat /dev/null > "$NOUPFILE"
   fi
 
-   QUERY=$(~/SEPLOS_MQTT/query_seplos_ha.sh 4201)
-   
+   QUERY=$(cat battery_status)
+ 
 # Find lowest and high value
                onlycells=$(echo $QUERY|awk '{print $1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16}')
 
@@ -52,39 +72,46 @@ highcellnumb=$(echo ${onlycells[@]} | awk 'BEGIN{RS=" ";} {print $1}' |nawk '{pr
 
 # Working on the bad data from the query script and skip send it to Mqtt server
 VAR="$(echo $QUERY|awk '{print $27}')"
-#VAR=${VAR/./,}
-#echo "$(date) - $VAR - $onlycells" >> $LOGNAME
-#    if [[ "$VAR" -gt 101 && "$VAR" -lt 0 ]]; then
-    if [[ "$onlycells" =~ "rror" ]]; then
-      echo "$(date) - Possible bad read data from BMS - Error 1" >> $LOGNAME
-#      echo "$(date) - Possible bad read data from BMS - Error 1"
-    else
-    
-      if [[ "$onlycells" =~ "Failed" ]]; then
-        echo "$(date) - Possible bad read data from BMS - Error 2" >> $LOGNAME
-#        echo "$(date) - Possible bad read data from BMS - Error 2"
-      else
-      
-        if (( $(echo "$VAR"'>'100 |bc -l) )); then
-          echo "$(date) - Possible bad read data from BMS - Error 3" >> $LOGNAME
-#          echo "$(date) - Possible bad read data from BMS - Error 3"
-        else
-        
-          if (( $(echo "$VAR"'<'1 |bc -l) )); then
-            echo "$(date) - Possible bad read data from BMS - Error 4" >> $LOGNAME
-#            echo "$(date) - Possible bad read data from BMS - Error 4"
-          else
-          
-            if [[ "$onlycells" =~ "~" ]]; then
-              echo "$(date) - Possible bad read data from BMS - Error 5" >> $LOGNAME
-#              echo "$(date) - Possible bad read data from BMS - Error 5"
-            else
-            
-              if [ "${VAR+x}" = x ] && [ -z "$VAR" ]; then
-                echo "$(date) - Possible bad read data from BMS - Error 6" >> $LOGNAME
-#                echo "$(date) - Possible bad read data from BMS - Error 6"
-              else
+CELL1=$(echo $QUERY|awk '{print $1}')
+CELL2=$(echo $QUERY|awk '{print $2}')
+CELL3=$(echo $QUERY|awk '{print $3}')
+CELL4=$(echo $QUERY|awk '{print $4}')
+CELL5=$(echo $QUERY|awk '{print $5}')
+CELL6=$(echo $QUERY|awk '{print $6}')
+CELL7=$(echo $QUERY|awk '{print $7}')
+CELL8=$(echo $QUERY|awk '{print $8}')
+CELL9=$(echo $QUERY|awk '{print $9}')
+CELL10=$(echo $QUERY|awk '{print $10}')
+CELL11=$(echo $QUERY|awk '{print $11}')
+CELL12=$(echo $QUERY|awk '{print $12}')
+CELL13=$(echo $QUERY|awk '{print $13}')
+CELL14=$(echo $QUERY|awk '{print $14}')
+CELL15=$(echo $QUERY|awk '{print $15}')
+CELL16=$(echo $QUERY|awk '{print $16}')
 
+	if [[ "$onlycells" =~ "rror" ]]; then
+		echo "$(date) - Possible bad read data from BMS - Error 1" >> $LOGNAME
+#		echo "$(date) - Possible bad read data from BMS - Error 1"
+	elif [[ "$onlycells" =~ "Failed" ]]; then
+		echo "$(date) - Possible bad read data from BMS - Error 2" >> $LOGNAME
+#		echo "$(date) - Possible bad read data from BMS - Error 2"
+	elif (( $(echo "$VAR"'>'100 |bc -l) )); then
+		echo "$(date) - Possible bad read data from BMS - Error 3" >> $LOGNAME
+#		echo "$(date) - Possible bad read data from BMS - Error 3"
+	elif (( $(echo "$VAR"'<'1 |bc -l) )); then
+		echo "$(date) - Possible bad read data from BMS - Error 4" >> $LOGNAME
+#		echo "$(date) - Possible bad read data from BMS - Error 4"
+	elif [[ "$onlycells" =~ "~" ]]; then
+		echo "$(date) - Possible bad read data from BMS - Error 5" >> $LOGNAME
+#		echo "$(date) - Possible bad read data from BMS - Error 5"
+	elif [ "${VAR+x}" = x ] && [ -z "$VAR" ]; then
+		echo "$(date) - Possible bad read data from BMS - Error 6" >> $LOGNAME
+#		echo "$(date) - Possible bad read data from BMS - Error 6"
+	else 
+	   
+	checkcellsvoltage
+		if [ $? = 0 ]; then
+				 
 # prepare MQTT argument
 mqtt_argument=$(printf "{\
 \"lowest_cell\":\"Cell $lowcellnumb - $lowcell mV\",\
@@ -94,22 +121,22 @@ mqtt_argument=$(printf "{\
 \"highest_cell_v\":\"$highcell\",\
 \"highest_cell_n\":\"$highcellnumb\",\
 \"difference\":\"$DIFF\",\
-\"cell01\":\"$(echo $QUERY|awk '{print $1}')\",\
-\"cell02\":\"$(echo $QUERY|awk '{print $2}')\",\
-\"cell03\":\"$(echo $QUERY|awk '{print $3}')\",\
-\"cell04\":\"$(echo $QUERY|awk '{print $4}')\",\
-\"cell05\":\"$(echo $QUERY|awk '{print $5}')\",\
-\"cell06\":\"$(echo $QUERY|awk '{print $6}')\",\
-\"cell07\":\"$(echo $QUERY|awk '{print $7}')\",\
-\"cell08\":\"$(echo $QUERY|awk '{print $8}')\",\
-\"cell09\":\"$(echo $QUERY|awk '{print $9}')\",\
-\"cell10\":\"$(echo $QUERY|awk '{print $10}')\",\
-\"cell11\":\"$(echo $QUERY|awk '{print $11}')\",\
-\"cell12\":\"$(echo $QUERY|awk '{print $12}')\",\
-\"cell13\":\"$(echo $QUERY|awk '{print $13}')\",\
-\"cell14\":\"$(echo $QUERY|awk '{print $14}')\",\
-\"cell15\":\"$(echo $QUERY|awk '{print $15}')\",\
-\"cell16\":\"$(echo $QUERY|awk '{print $16}')\",\
+\"cell01\":\"$CELL1\",\
+\"cell02\":\"$CELL2\",\
+\"cell03\":\"$CELL3\",\
+\"cell04\":\"$CELL4\",\
+\"cell05\":\"$CELL5\",\
+\"cell06\":\"$CELL6\",\
+\"cell07\":\"$CELL7\",\
+\"cell08\":\"$CELL8\",\
+\"cell09\":\"$CELL9\",\
+\"cell10\":\"$CELL10\",\
+\"cell11\":\"$CELL11\",\
+\"cell12\":\"$CELL12\",\
+\"cell13\":\"$CELL13\",\
+\"cell14\":\"$CELL14\",\
+\"cell15\":\"$CELL15\",\
+\"cell16\":\"$CELL16\",\
 \"cell_temp1\":\"$(echo $QUERY|awk '{print $17}')\",\
 \"cell_temp2\":\"$(echo $QUERY|awk '{print $18}')\",\
 \"cell_temp3\":\"$(echo $QUERY|awk '{print $19}')\",\
@@ -124,14 +151,12 @@ mqtt_argument=$(printf "{\
 \"soh\":\"$(echo $QUERY|awk '{print $30}')\",\
 \"port_voltage\":\"$(echo $QUERY|awk '{print $31}')\"\
 }")
+
 # send MQTT message with all parameters
-        mosquitto_pub -h $MQTTHOST -u $MQTTUSER -P $MQTTPASWD -t "homeassistant/sensor/"$TOPIC"_"$id_prefix"" -m "$mqtt_argument"
-#        echo "mqtt sent"
-            fi
-          fi
-        fi
-      fi
-    fi
-  fi
-  sleep $TELEPERIOD
+	mosquitto_pub -h $MQTTHOST -u $MQTTUSER -P $MQTTPASWD -t "homeassistant/sensor/"$TOPIC"_"$id_prefix"" -m "$mqtt_argument"
+#	echo "mqtt sent"
+
+		fi
+	fi
+	sleep $TELEPERIOD
 done
